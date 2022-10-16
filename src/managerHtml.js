dac5cc3572d2a9e62fa85e41b72df7731889e07d
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-10-16 00:30:33 -0400

    created intern and engineer html templates and fixed the manager

--- src/managerHtml.js
+++ src/managerHtml.js
@@ -4,11 +4,10 @@ function manageHtml(manager){
       <div class="card-body">
         <h5 class="card-title">Manger</h5>
         <ul class="list-group">
-          <li class="list-group-item">name: ${manager.name}</li>
-          <li class="list-group-item">id:  ${manager.id}</li>
-          <li class="list-group-item">email: ploy3_98@yahoo.com</li>
-          <li class="list-group-item">OfficeNumber: 123</li>
-           
+          <li class="list-group-item">Name: ${manager.name}</li>
+          <li class="list-group-item">ID:  ${manager.id}</li>
+          <li class="list-group-item">Email:${manager.email}</li>
+          <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
         </ul>
       </div>
     </div>
